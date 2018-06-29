6d8502dd211969615126e123724d7982c2005c3d
Edgar Peixoto <edgarpeixoto14@gmail.com>
2018-06-29 08:33:12 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var parasiticNumber = require('./');
 
-assert.strictEqual(parasiticNumber(1,1,100), '1');
-assert.strictEqual(parasiticNumber(2,2,100), '105263157894736842');
-assert.strictEqual(parasiticNumber(3,3,100), '1034482758620689655172413793');
-assert.strictEqual(parasiticNumber(3,5,100), '1724137931034482758620689655');
+assert.strictEqual(parasiticNumber(1,1), '1');
+assert.strictEqual(parasiticNumber(2,2), '105263157894736842');
+assert.strictEqual(parasiticNumber(3,3), '1034482758620689655172413793');
+assert.strictEqual(parasiticNumber(3,5), '1724137931034482758620689655');
