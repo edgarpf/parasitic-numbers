ef3a04533c91ef20492c1db3eda175ca8bffecd0
edgarpf <edgarpeixoto_06@hotmail.com>
2018-06-29 00:15:37 -0300

    function done

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = function(n, k, precision = 1000) {
+	return fractionToDecimal(k, 10*n - 1, precision);
+}
+	
+function fractionToDecimal(n, d, precision) {
+	var prime_factors = require('prime-factors')
+    var pFS = prime_factors(d);
+	
+    for (var i = 0; i < pFS.length; i++) {
+
+        if (pFS[i] !== 2 && pFS[i] !== 5) {
+
+            var output = new Array();
+            var ns = new Array();
+            
+            for (var i = 0; i < precision; i++) {
+                var temp2 = parseInt(n / d);
+                
+                if (ns[n] === undefined) {
+                    ns[n] = i;
+                } else {
+					return output.slice(ns[n]).join('');
+                }
+                
+                output.push(temp2);
+                
+                var n = n % d;
+                
+                n += "0";
+            }            
+        }
+    }
+}
---
+++ test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var parasiticNumber = require('./');
+
+console.log(parasiticNumber(1,1));
+console.log(parasiticNumber(2,2));
+console.log(parasiticNumber(3,3));
+console.log(parasiticNumber(4,4));
+console.log(parasiticNumber(5,5));
+console.log(parasiticNumber(6,6));
+console.log(parasiticNumber(7,7));
+console.log(parasiticNumber(8,8));
+console.log(parasiticNumber(9,9));
